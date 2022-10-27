eacf921f9419629cc720c0ec5d662f2b8d4240a5
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-10-27 21:47:02 +0200

    add create module

---
+++ js/modules/createModules.js
@@ -0,0 +1,33 @@
+export class CreateElements{
+     createElement(element,attributes,text=''){
+        const elemName=document.createElement(element);
+        if(Object.keys(attributes).length !== 0)
+             this.setAttr(elemName,attributes);
+        if(text)
+           this.addInnerText(elemName,text)
+        return elemName;
+    }
+    
+     setAttr(element,attributes){
+       Object.keys(attributes).forEach(key => 
+        element.setAttribute(key,attributes[key]));
+    }
+    
+     addInnerText(element,text){
+        element.append(text);
+     }
+    
+     createMeal(mealName,mealImg)
+    {
+       const h2=this.createElement('h2',{},mealName)
+       const div=this.createElement('div',{class:"mealName rounded-3"},h2);
+       const img=this.createElement('img',{src:mealImg,alt:mealName,class:'w-100 rounded-3'});
+       const mealCard=this.createElement('div',{class:"position-relative mealCard"});
+       mealCard.appendChild(img)
+       mealCard.appendChild(div);
+       const colDiv=this.createElement('div',{class:"col-lg-3 col-md-6"});
+       colDiv.appendChild(mealCard);
+       console.log(colDiv);
+    }
+    
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 import {navAnimation} from './modules/navModule.js';
+import { CreateElements } from './modules/createModules.js';
 
 navAnimation();
+const create=new CreateElements();
+const displayReceipes=document.querySelector('#displayReceipes');
+create.createMeal('pizza','images/logo.png');
+
+
 
