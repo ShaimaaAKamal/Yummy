f81c0f114ec7a3d60c91d4a4516bcffc76cabfa8
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-10-27 15:59:21 +0200

    nav animation effect

--- js/script.js
+++ js/script.js
@@ -4,12 +4,11 @@ $('#menuIcon').click(function(){
     $('.mainNav').removeClass('d-none');
     $('#closeMenuIcon').removeClass('d-none');
     $(this).addClass('d-none');
-
-    $('#searchLink').animate({'bottom':'0px','opacity':1},400);
-    $('#categoriesLink').animate({'bottom':'0px','opacity':1},500);
-    $('#areaLink').animate({'bottom':'0px','opacity':1},600)
-    $('#ingredientsLink').animate({'bottom':'0px','opacity':1},700)
-    $('#contactLink').animate({'bottom':'0px','opacity':1},800)
+    $('#searchLink').delay(150).animate({'bottom':'0px','opacity':1},400);
+    $('#categoriesLink').delay(150).animate({'bottom':'0px','opacity':1},500);
+    $('#areaLink').delay(150).animate({'bottom':'0px','opacity':1},600)
+    $('#ingredientsLink').delay(150).animate({'bottom':'0px','opacity':1},700)
+    $('#contactLink').delay(150).animate({'bottom':'0px','opacity':1},800)
 
 })
 
@@ -17,11 +16,11 @@ $('#closeMenuIcon').click(function(){
     $('.mainNav').addClass('d-none');
     $('#menuIcon').removeClass('d-none');
     $(this).addClass('d-none');
-    $('#searchLink').css({'bottom':'-34vh','opacity':0});
-    $('#categoriesLink').css({'bottom':'-34vh','opacity':0});
-    $('#areaLink').css({'bottom':'-34vh','opacity':0});
-    $('#ingredientsLink').css({'bottom':'-34vh','opacity':0});
-    $('#contactLink').css({'bottom':'-34vh','opacity':0});
+    $('#searchLink').css({'bottom':'-24vh','opacity':0});
+    $('#categoriesLink').css({'bottom':'-24vh','opacity':0});
+    $('#areaLink').css({'bottom':'-24vh','opacity':0});
+    $('#ingredientsLink').css({'bottom':'-24vh','opacity':0});
+    $('#contactLink').css({'bottom':'-24vh','opacity':0});
 })
 
 
