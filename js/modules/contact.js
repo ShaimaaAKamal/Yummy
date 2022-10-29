ce0ede5a2cbdff757789fe14de5dc65f56e6335f
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-10-29 12:35:45 +0200

    finish of contact page

--- js/modules/contact.js
+++ js/modules/contact.js
@@ -2,6 +2,7 @@ import { Validation } from "./validation.js";
 import { General } from "./generalModules.js";
 const validate=new Validation();
 const general =new General();
+let count=0
 
 export class HandleContact{
      handleName(input){
@@ -18,60 +19,51 @@ export class HandleContact{
    handleAge(input){
     this.handle(input,validate.validateAge,'#age');
    }
+
    handlePassword(input,elementSelector){
-    if(this.checkPassowrd())
-       this.handle(input,validate.validatePassword,elementSelector);
+    if(elementSelector === '#repassword') count++;
+    this.handle(input,validate.validatePassword,elementSelector);
    }
    checkPassowrd(){
       const password=$(`#password`).val();
       const repassword=$(`#repassword`).val();
       const repaswordSiblings=$('#repassword').nextAll();
       const repasswordInput=document.querySelector('#repassword');
-      if(password !== repassword && repassword !== ''){
-        this.displayInValidMessage(repaswordSiblings,repasswordInput);
-        return false;
+      if(count !=0){
+            if(password !== repassword){
+                this.displayInValidMessage(repaswordSiblings,repasswordInput);
+                return false;
+                }
+              if(validate.validatePassword(repassword)){
+                this.displayValidMessages(repaswordSiblings,repasswordInput);
+              }
+              else this.displayInValidMessage(repaswordSiblings,repasswordInput);
+
       }
-      this.displayValidMessages(repaswordSiblings,repasswordInput);
+   
       return true;
    }
 
 
    handle(input,func,selector){
     const siblings=$(selector).nextAll();
-    if(func(input.value)){  
+    if(func(input.value)){
+       if(selector === '#repassword'){
+         this.checkPassowrd(); return;
+       }
+       if(selector === '#password'){
+        this.checkPassowrd();
+      }
+       
        this.displayValidMessages(siblings,input)
     }
     else{
+        if(selector === '#password'){
+            this.checkPassowrd();
+          }
         this.displayInValidMessage(siblings,input);
     }
    }
-
-//    handle(input,func,selector){
-//     const siblings=$(selector).nextAll();
-//     if(func(input.value)){
-//         if(selector === '#repassword' && input.value !== $('#password').val()){
-//             this.displayInValidMessage(siblings,input);
-//             return;
-//          }
-//          if(selector === '#password' ){
-//             const repaswordSiblings=$('#repassword').nextAll();
-//             const repassword=document.querySelector('#repassword')
-//             if(input.value === $('#repassword').val()){
-//                 this.displayValidMessages(repaswordSiblings,repassword);
-//             }
-//             else{
-//                 this.displayInValidMessage(repaswordSiblings,repassword);
-//             }
-     
-           
-//          }
-         
-//        this. displayValidMessages(siblings,input)
-//     }
-//     else{
-//         this.displayInValidMessage(siblings,input);
-//     }
-//    }
 
    displayInValidMessage(siblings,input){
          general.showElements([siblings[1],siblings[2]]);
--- js/script.js
+++ js/script.js
@@ -17,7 +17,8 @@ const menuIcon=document.querySelector('#menuIcon');
 const closeMenuIcon=document.querySelector('#closeMenuIcon');
 const lengthError=document.querySelector('#lengthError');
 const contact=document.querySelector('.contact');
-
+const checkIcons=document.querySelectorAll('.checkIcon');
+const errorMsg=document.querySelectorAll('.errorMsg');
 navAnimation();
 displayDataClass.getMealsByCategory('chicken',displayMeals);
 
@@ -59,14 +60,14 @@ $("#searchLink").click(function(e){
 $("#contactLink").click(function(e){
     e.preventDefault();
     $('.mainNav').css({'width':'0px','opacity':'0'}).removeClass('ps-4 pe-1');
-    general.hideElements([displayMeals,closeMenuIcon,displayCategories,displayIngredients,mealDetails,displayAreas,search,noResults]);
+    general.hideElements([displayMeals,closeMenuIcon,displayCategories,displayIngredients,mealDetails,displayAreas,search,noResults,...checkIcons,...errorMsg]);
     general.showElements([contact,menuIcon]);
-    $('#name').keyup((e)=>handleContact.handleName(e.target)).blur(e=> e.target.style.color='#fff');
-    $('#email').keyup((e)=>handleContact.handleEmail(e.target)).blur(e=> e.target.style.color='#fff');
-    $('#phone').keyup((e)=>handleContact.handlePhone(e.target)).blur(e=> e.target.style.color='#fff');
-    $('#age').keyup((e)=>handleContact.handleAge(e.target)).blur(e=> e.target.style.color='#fff');
-    $('#password').keyup((e)=>handleContact.handlePassword(e.target,'#password')).blur(e=> e.target.style.color='#fff');
-    $('#repassword').keyup((e)=>handleContact.handlePassword(e.target,'#repassword')).blur(e=> e.target.style.color='#fff');
+    $('#name').val('').css('borderColor','rgba(255, 255, 255, 0.7)').keyup((e)=>handleContact.handleName(e.target)).blur(e=> e.target.style.color='#fff');
+    $('#email').val('').css('borderColor','rgba(255, 255, 255, 0.7)').keyup((e)=>handleContact.handleEmail(e.target)).blur(e=> e.target.style.color='#fff');
+    $('#phone').val('').css('borderColor','rgba(255, 255, 255, 0.7)').keyup((e)=>handleContact.handlePhone(e.target)).blur(e=> e.target.style.color='#fff');
+    $('#age').val('').css('borderColor','rgba(255, 255, 255, 0.7)').keyup((e)=>handleContact.handleAge(e.target)).blur(e=> e.target.style.color='#fff');
+    $('#password').val('').css('borderColor','rgba(255, 255, 255, 0.7)').keyup((e)=>handleContact.handlePassword(e.target,'#password')).blur(e=> e.target.style.color='#fff');
+    $('#repassword').val('').css('borderColor','rgba(255, 255, 255, 0.7)').keyup((e)=>handleContact.handlePassword(e.target,'#repassword')).blur(e=> e.target.style.color='#fff');
 })
 
 $('#searchByName').keyup(function(e){
