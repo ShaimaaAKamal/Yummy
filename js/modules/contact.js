eabdb6534332f8f1f450a56e46ad01b4339b4e63
Shaimaa Kamal <Shaimaakamal039@gmail.com>
2022-10-29 11:08:35 +0200

    check equal password

--- js/modules/contact.js
+++ js/modules/contact.js
@@ -19,22 +19,59 @@ export class HandleContact{
     this.handle(input,validate.validateAge,'#age');
    }
    handlePassword(input,elementSelector){
-    this.handle(input,validate.validatePassword,elementSelector);
+    if(this.checkPassowrd())
+       this.handle(input,validate.validatePassword,elementSelector);
    }
+   checkPassowrd(){
+      const password=$(`#password`).val();
+      const repassword=$(`#repassword`).val();
+      const repaswordSiblings=$('#repassword').nextAll();
+      const repasswordInput=document.querySelector('#repassword');
+      if(password !== repassword && repassword !== ''){
+        this.displayInValidMessage(repaswordSiblings,repasswordInput);
+        return false;
+      }
+      this.displayValidMessages(repaswordSiblings,repasswordInput);
+      return true;
+   }
+
 
    handle(input,func,selector){
     const siblings=$(selector).nextAll();
-    if(func(input.value)){
-        if(selector ==='repassword' && input.value !== $('#password').val()){
-            this.displayInValidMessage(siblings,input);
-            return;
-        }
-       this. displayValidMessages(siblings,input)
+    if(func(input.value)){  
+       this.displayValidMessages(siblings,input)
     }
     else{
         this.displayInValidMessage(siblings,input);
     }
    }
+
+//    handle(input,func,selector){
+//     const siblings=$(selector).nextAll();
+//     if(func(input.value)){
+//         if(selector === '#repassword' && input.value !== $('#password').val()){
+//             this.displayInValidMessage(siblings,input);
+//             return;
+//          }
+//          if(selector === '#password' ){
+//             const repaswordSiblings=$('#repassword').nextAll();
+//             const repassword=document.querySelector('#repassword')
+//             if(input.value === $('#repassword').val()){
+//                 this.displayValidMessages(repaswordSiblings,repassword);
+//             }
+//             else{
+//                 this.displayInValidMessage(repaswordSiblings,repassword);
+//             }
+     
+           
+//          }
+         
+//        this. displayValidMessages(siblings,input)
+//     }
+//     else{
+//         this.displayInValidMessage(siblings,input);
+//     }
+//    }
 
    displayInValidMessage(siblings,input){
          general.showElements([siblings[1],siblings[2]]);
